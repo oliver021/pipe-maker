e005f162d4fa5eae4fbfa735d76cf15e95c96591
oliver021 <oliver021val@gmail.com>
2021-02-20 18:50:12 -0500

    add lint check for typescript source

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,2 @@
-export declare type PipeFunc<T> = (arg: T, next: Function) => void;
-export default class Pipeline<TInitial> {
-    handler: PipeFunc<TInitial>;
-    private _pipes;
-    constructor(pipe: PipeFunc<TInitial>, pipes?: Array<Pipeline<TInitial>>);
-    pipe(pipe: PipeFunc<TInitial>): Pipeline<TInitial>;
-    run(arg: TInitial): void;
-    private _run;
-}
-export declare function usePipeline<T>(first: PipeFunc<T>): Pipeline<T>;
+export * from "./pipeline";
+export * from "./PipelineMuted";
